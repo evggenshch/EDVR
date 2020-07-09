2b63f174da2b7f2dca55b7c49d803796eb5da802
evgenij.scherbakov <evggenshch@gmail.com>
2020-07-09 14:58:17 +0600

    fixed error with negative index select

--- codes/data/util.py
+++ codes/data/util.py
@@ -155,6 +155,8 @@ def index_generation(crt_i, max_n, N, padding='reflection'):     #   HERE MISMAT
                 raise ValueError('Wrong padding mode')
         else:
             add_idx = i
+        if add_idx < 0:
+            add_idx = 0
         return_l.append(add_idx)
     return return_l
 
